d922f7a083a9c123a7e3bf79c65aec6e34181518
proweb4all <proweb4all@narod.ru>
2019-04-18 23:38:17 +0300

    54

--- src/index.js
+++ src/index.js
@@ -1,25 +1,40 @@
-const getResource = async (url) => {
-  const res = await fetch(url);
-  // if (!res.ok) {
-  //   throw new Error(`Ошибка fetch ${url}, статус ${res.status}`);
-  // };
-  const body = await res.json();
-  return body;
-};
-getResource('https://swapi.co/api/people/16568341/')
-.then((body) => {
-  console.log(body);
-})
-.then((err) => {
-  console.log('Ошибка', err)
+class SwapiService {
+  _apiBase = 'https://swapi.co/api';
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
+    if (!res.ok) {
+      throw new Error(`Ошибка fetch ${url}, статус ${res.status}`);
+    };    
+    return await res.json(); 
+  };
+  async getAllPeople() {
+    const res = await this.getResource(`/people/`);
+    return res.results;
+  };
+  getPerson(id) {
+    return this.getResource(`/people/${id}`);
+  };
+  async getAllPlanets() {
+    const res = await this.getResource(`/planets/`);
+    return res.results;
+  };
+  getPlanet(id) {
+    return this.getResource(`/planets/${id}`);
+  };
+  async getAllStarships() {
+    const res = await this.getResource(`/starchips/`);
+    return res.results;
+  };
+  getStarship(id) {
+    return this.getResource(`/starchips/${id}`);
+  };
+}
+
+const swapi = new SwapiService();
+swapi.getAllPeople().then((people) => {
+  people.forEach(p => {
+    console.log(p.name);
+  });
 });
 
-
-// fetch('https://swapi.co/api/people/1/')
-//   .then((res) => {
-//     return res.json();
-//     console.log('Ответ', res.status);
-//   })
-//   .then((body) => {
-//     console.log(body);
-//   })
+///getResource('https://swapi.co/api/people/16568341/')
