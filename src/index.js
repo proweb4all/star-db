df7fed604edeec704db592bffdad16648d71fcdf
proweb4all <proweb4all@narod.ru>
2019-04-18 17:17:18 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
+const getResource = async (url) => {
+  const res = await fetch(url);
+  // if (!res.ok) {
+  //   throw new Error(`Ошибка fetch ${url}, статус ${res.status}`);
+  // };
+  const body = await res.json();
+  return body;
+};
+getResource('https://swapi.co/api/people/16568341/')
+.then((body) => {
+  console.log(body);
+})
+.then((err) => {
+  console.log('Ошибка', err)
+});
 
-// ReactDOM.render(<App />, document.getElementById('root'));
 
-// serviceWorker.unregister();
-console.log('Star-DB');
+// fetch('https://swapi.co/api/people/1/')
+//   .then((res) => {
+//     return res.json();
+//     console.log('Ответ', res.status);
+//   })
+//   .then((body) => {
+//     console.log(body);
+//   })
