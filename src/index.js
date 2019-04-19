77338242677e8588acf907a5376eab11f338b449
proweb4all <proweb4all@narod.ru>
2019-04-19 13:39:16 +0300

    57

--- src/index.js
+++ src/index.js
@@ -1,40 +1,6 @@
-class SwapiService {
-  _apiBase = 'https://swapi.co/api';
-  async getResource(url) {
-    const res = await fetch(`${this._apiBase}${url}`);
-    if (!res.ok) {
-      throw new Error(`Ошибка fetch ${url}, статус ${res.status}`);
-    };    
-    return await res.json(); 
-  };
-  async getAllPeople() {
-    const res = await this.getResource(`/people/`);
-    return res.results;
-  };
-  getPerson(id) {
-    return this.getResource(`/people/${id}`);
-  };
-  async getAllPlanets() {
-    const res = await this.getResource(`/planets/`);
-    return res.results;
-  };
-  getPlanet(id) {
-    return this.getResource(`/planets/${id}`);
-  };
-  async getAllStarships() {
-    const res = await this.getResource(`/starchips/`);
-    return res.results;
-  };
-  getStarship(id) {
-    return this.getResource(`/starchips/${id}`);
-  };
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-const swapi = new SwapiService();
-swapi.getAllPeople().then((people) => {
-  people.forEach(p => {
-    console.log(p.name);
-  });
-});
-
-///getResource('https://swapi.co/api/people/16568341/')
+ReactDOM.render(<App />,
+  document.getElementById('root'));
